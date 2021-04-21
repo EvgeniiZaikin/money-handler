55ae5ee989bb2d941e1105aa95c1a24eeaf47a48
Evgenii Zaikin <evgenii.zaikin2607@gmail.com>
2021-04-21 11:25:14 +0300

    Create basic next app. Added typescript.

--- README.md
+++ README.md
@@ -9,3 +9,6 @@
 2. React
 3. Redux
 4. ReduxSaga
+5. Graphql
+6. AWS Serverless
+7. Material-UI
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const IndexPage: NextPage = () => (
+  <div>
+    <Head>
+      <title>Money handler - ваш личный финансовый управляющий</title>
+    </Head>
+    <h1>Hello Money Handler!</h1>
+  </div>
+);
+
+export default IndexPage;
