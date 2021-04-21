540b6f54dfcfff139ce23c25ec58cf6a0a0c46ff
Evgenii Zaikin <evgenii.zaikin2607@gmail.com>
2021-04-21 17:55:32 +0300

    Create basic index page with material-ui. Fix bug with serverstyles rendering.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,33 @@
+import { useEffect } from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 
 import { wrapper } from 'store';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Head>
-      <meta
-        name="viewport"
-        content={`width=device-width, minimum-scale=1.0, initial-scale=1.0, 
-                            maximum-scale=1.0, user-scalable=no, viewport-fit=cover`}
-      />
-      <title>Money handler app</title>
-      <link rel="shortcut icon" href="/favicon.ico" />
-    </Head>
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
-    <Component {...pageProps} />
-  </>
-);
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content={`width=device-width, minimum-scale=1.0, initial-scale=1.0, 
+                            maximum-scale=1.0, user-scalable=no, viewport-fit=cover`}
+        />
+        <title>Money handler - ваш личный финансовый управляющий</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default wrapper.withRedux(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,27 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { NextPage } from 'next';
 import Head from 'next/head';
+import uniqid from 'uniqid';
+
+import Fab from '@material-ui/core/Fab';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 
 import { getIsAuth } from 'store/reducers/user/selectors';
 import { login } from 'store/reducers/user';
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    block: {
+      textAlign: 'center',
+    },
+  })
+);
+
 const IndexPage: NextPage = () => {
+  const classes = useStyles();
+
   const isAuth: boolean = useSelector(getIsAuth);
   const dispatch = useDispatch();
 
@@ -16,11 +32,18 @@ const IndexPage: NextPage = () => {
 
   return (
     <div>
-      <Head>
-        <title>Money handler - ваш личный финансовый управляющий</title>
-      </Head>
       <h1>Hello Money Handler!</h1>
       <p>User login status: {`${isAuth}`}</p>
+      <Grid container spacing={2}>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item: number) => (
+          <Grid key={uniqid()} item xs={4} className={classes.block}>
+            <Fab color="primary">{item}</Fab>
+          </Grid>
+        ))}
+      </Grid>
+      <Box className={classes.block} paddingTop="1rem">
+        <Fab color="primary">0</Fab>
+      </Box>
     </div>
   );
 };
