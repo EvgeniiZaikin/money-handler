f2aedb06c9be76659cbe2b189d22afe6f194109a
Evgenii Zaikin <evgenii.zaikin2607@gmail.com>
2021-05-18 16:38:33 +0300

    Added tests. Create basic demo test.

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+      tsConfig: 'jest.tsconfig.json',
+    },
+  },
+  coveragePathIgnorePatterns: ['/node_modules/', 'enzyme.js'],
+  moduleNameMapper: {
+    'components/(.*)': '<rootDir>/components/$1',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['tsx', 'ts', 'js', 'jsx', 'json', 'node'],
+};
